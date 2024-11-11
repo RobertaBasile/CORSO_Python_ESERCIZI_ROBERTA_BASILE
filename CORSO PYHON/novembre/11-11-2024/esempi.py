43c6197b0d375c5a485e6231b92829be537ca9e4
RobertaBasile <robertabasile1997@gmail.com>
2024-11-11 10:13:42 +0100

    Update esempi.py

--- CORSO PYHON/novembre/11-11-2024/esempi.py
+++ CORSO PYHON/novembre/11-11-2024/esempi.py
@@ -4,5 +4,8 @@
 
 # with open ("prova.txt", "x")
 
-with open ("prova.txt", "w") as file:
-    file.write("ciao")
+# with open ("prova.txt", "w") as file:
+#     file.write("ciao")
+with open ("prova.txt", "r") as file:
+     contenuto = file.readlines()
+print(contenuto)
